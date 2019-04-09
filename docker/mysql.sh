c509f4970cbc7ed10c0b1cf15d1aab94e84c0e6f
xihefeng <xihefeng@users.noreply.github.com>
2019-04-09 20:40:08 +0800

    Create mysql.sh

---
+++ docker/mysql.sh
@@ -0,0 +1,1 @@
+docker run --name mysql -e TZ=Asia/Shanghai -v /root/DATA:/var/lib/mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=1234qwerASDF@2 -d mysql:5.7  --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci --default-time_zone='+8:00'
