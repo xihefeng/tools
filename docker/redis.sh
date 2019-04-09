102e64802184ce044090e8b5a90a9493a6d6656d
xihefeng <xihefeng@users.noreply.github.com>
2019-04-09 20:43:37 +0800

    Update redis.sh

--- docker/redis.sh
+++ docker/redis.sh
@@ -1,1 +1,1 @@
-docker run -p 6379:6379 -v $PWD/redisdata:/data  -d redis:3.2 redis-server --appendonly yes
+docker run -p 6379:6379 -v $PWD/redisdata:/data  -d redis:4.0 redis-server --appendonly yes
